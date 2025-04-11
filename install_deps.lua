a7c500938a1bc6022e90d4910e8a09949aa1f727
DaApppooo <appo.tcp@gmail.com>
2025-04-11 20:41:26 +0200

    -

--- install_deps.lua
+++ install_deps.lua
@@ -1,146 +1,76 @@
 -- Small list of dependencies by name:
 -- TARGET={LINUX, WIN}
---  PROG=aac
---   raylib by raysan5 on github under zlib License
---   nfd by by mlabbe on github under zlib license
---   libspng by (a bunch of people) on libpng.org under the BSDL3.0
---   clay by nicbarker on github under zlib license
--- TARGET={LINUX,WIN,ANDROID}
---  PROG=obz2cobz
---   zip by kuba-- on github under MIT license
---   cJSON by DaveGamble on github under MIT license
---   plutosvg by sammycage under MIT license
---   stb_image by nothings under public domain
-require("shared")
-
-load_os() -- declares TARGET
+--  raylib by raysan5 on github under zlib License
+--  nfd by by mlabbe on github under zlib license
+--  libspng by (a bunch of people) on libpng.org under the BSDL3.0
+--  clay by nicbarker on github under zlib license
+require("src/shared/shared")
+load_os()
 RAYLIB_VERSION = "5.5"
-CJSON_VERSION = "1.7.18"
-ZIP_VERSION = "0.3.3"
-PLUTOSVG_VERSION = "0.0.6"
-PLUTOVG_VERSION = "1.0.0" -- dependency of plutosvg
-ONLY = nil
-for _, a in pairs(arg) do
-  if startswith(a, "target=") then
-    TARGET = string.upper(string.sub(a, string.find(a, "=")+1))
-    assert(
-       TARGET == "LINUX"
-    or TARGET == "WIN"
-    or TARGET == "IOS"
-    or TARGET == "ANDROID",
-      TARGET .. " is and unknown target. Expected LINUX, WIN, IOS or ANDROID."
-    )
-  elseif startswith(a, "only=") then
-    ONLY = string.sub(a, string.len("only=")+1)
-  end
-end
-TEMP_FOLDERS = { "raylib", "plutosvg", "nfd", "nativefiledialog", "libspng", "iniparser" }
-function inst(title)
-  return ONLY == nil or title == ONLY:lower()
-end
+CLAY_VERSION = "0.13"
+TEMP_FOLDERS = { "raylib", "nfd", "nativefiledialog", "libspng", "iniparser" }
+parse_args()
 
 if TARGET == "IOS" or TARGET == "ANDROID" then
   error("Targets IOS and ANDROID are not yet supported.")
 end
 
-if not exists("lib") then
-  shell("mkdir lib")
-end
-if not exists("include") then
-  shell("mkdir include")
-end
-if not exists("licenses") then
-  shell("mkdir licenses")
-end
+ensure_folder("lib")
+ensure_folder("include")
+ensure_folder("licenses")
 
 print("Check that compiler is available...")
 if not os.execute("gcc --version") then
   error("Missing gcc. Please go install gcc.")
 end
 
-if inst("clay") then
+function inst_clay()
   print("Updating/Installing clay.h ...")
   if TARGET == "LINUX" then
-    shell("wget https://github.com/nicbarker/clay/releases/download/v0.13/clay.h")
-  elseif TARGET == "WIN" then
-    os.exit(1)
-    -- shell("wget https://raw.githubusercontent.com/nicbarker/clay/refs/heads/main/clay.h")
+    shell("wget https://github.com/nicbarker/clay/releases/download/v"..CLAY_VERSION.."/clay.h")
+  else
+    todo()
   end
   mv("clay.h", "src/clay.h")
 end
 
-print("Grab latest release from raylib for target=" .. TARGET)
-if TARGET == "LINUX" then
-  if inst("raylib") then
-    print("Download and install raylib (for app only)...")
+function inst_nfd()
+  print("Download, compile and move nfd...")
+  if TARGET == "LINUX" then
+    shell("git clone https://github.com/mlabbe/nativefiledialog.git")
+    shell("cd nativefiledialog/build/gmake_linux && make config=release_x64")
+    mv("nativefiledialog/build/lib/Release/x64/libnfd.a", "lib/")
+    rm("nativefiledialog")
+  else
+    todo()
+  end
+end
+function inst_libspng()
+  print("Download, compile and move libspng...")
+  if TARGET == "LINUX" then
+    shell("git clone https://github.com/randy408/libspng.git")
+    shell("gcc -fPIC libspng/spng/spng.c -shared -o lib/libspng.so")
+    rm("libspng")
+  else
+    todo()
+  end
+end
+function inst_raylib()
+  print("Download and install raylib (for app only)...")
+  if TARGET == "LINUX" then
     shell("wget https://github.com/raysan5/raylib/releases/download/" .. RAYLIB_VERSION .. "/raylib-" .. RAYLIB_VERSION .. "_linux_amd64.tar.gz")
     shell("tar -xvzf raylib-" .. RAYLIB_VERSION .. "_linux_amd64.tar.gz")
     mv("raylib-" .. RAYLIB_VERSION .. "_linux_amd64/lib/libraylib.so*", "lib/")
     mv("raylib-" .. RAYLIB_VERSION .. "_linux_amd64/include/*", "include/")
     rm("raylib-" .. RAYLIB_VERSION .. "_linux_amd64.tar.gz")
     rm("-r raylib-" .. RAYLIB_VERSION .. "_linux_amd64")
+  else
+    todo()
   end
-
-  if inst("nfd") then
-    print("Download, compile and move nfd...")
-    shell("git clone https://github.com/mlabbe/nativefiledialog.git")
-    shell("cd nativefiledialog/build/gmake_linux && make config=release_x64")
-    mv("nativefiledialog/build/lib/Release/x64/libnfd.a", "lib/")
-    rm("nativefiledialog")
-  end
-
-  if inst("libspng") then
-    print("Download, compile and move libspng...")
-    shell("git clone https://github.com/randy408/libspng.git")
-    shell("gcc -fPIC libspng/spng/spng.c -shared -o lib/libspng.so")
-    rm("libspng")
-  end
-
-  if inst("zip") then
-    print("Download, compile and move zip...")
-    shell("wget https://github.com/kuba--/zip/archive/refs/tags/v"..ZIP_VERSION..".tar.gz")
-    shell("tar -xzvf v"..ZIP_VERSION..".tar.gz")
-    shell("mkdir -p zip-"..ZIP_VERSION.."/build")
-    shell("cd zip-"..ZIP_VERSION.."/build && cmake -DBUILD_SHARED_LIBS=true -DBUILD_STATIC_PIC=OFF .. && cmake --build .")
-    mv("zip-"..ZIP_VERSION.."/build/*.so", "lib/")
-    mv("zip-"..ZIP_VERSION.."/LICENSE.txt", "licenses/zip.txt")
-    rm("v"..ZIP_VERSION..".tar.gz*")
-    rm("zip-"..ZIP_VERSION)
-  end
-
-  if inst("cjson") then
-    print("Download and move cJSON...")
-    shell("wget https://github.com/DaveGamble/cJSON/archive/refs/tags/v"..CJSON_VERSION..".tar.gz")
-    shell("tar -xzvf v"..CJSON_VERSION..".tar.gz")
-    mv("cJSON-"..CJSON_VERSION.."/cJSON.*", "obz2cobz/")
-    mv("cJSON-"..CJSON_VERSION.."/LICENSE", "licenses/cJSON.txt")
-    rm("v"..CJSON_VERSION..".tar.gz*")
-    rm("cJSON-"..CJSON_VERSION)
-  end
-
-  if inst("plutosvg") then
-    print("Download, compile and move plutosvg...")
-    if exists("plutosvg") then
-      rm("plutosvg")
-    end
-    shell("git clone --recursive https://github.com/sammycage/plutosvg.git")
-    shell("cd plutosvg && cmake -B build . && cmake --build build")
-    mv("plutosvg/source/*", "include/")
-    mv("plutosvg/plutovg/include/plutovg.h", "include/")
-    -- mv("plutosvg/build/plutovg/*.a", "include") -- reactivate this line if plutosvg.a is not enough
-    mv("plutosvg/build/*.so", "lib/")
-    mv("plutosvg/LICENSE", "licenses/plutosvg.txt")
-    rm("plutosvg")
-  end
-
-  if inst("stb_image") then
-    print("Download and move stb_image...")
-    shell("wget https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_image.h")
-    mv("stb_image.h", "obz2cobz/stb_image.h")
-  end
-
-  if inst("iniparser") then
-    print("Download, compile and move iniParser 4...")
+end
+function inst_iniparser()
+  print("Download, compile and move iniParser 4...")
+  if TARGET == "LINUX" then
     if exists("iniparser") then
       rm("iniparser")
     end
@@ -150,10 +80,13 @@ if TARGET == "LINUX" then
     mv("iniparser/build/*.so*", "lib/")
     mv("iniparser/src/*.h", "include/")
     rm("iniparser")
+  else
+    todo()
   end
-
-  if inst("piper") then
-    print("Download, compile and move (lib)Piper...")
+end
+function inst_piper()
+  print("Download, compile and move (lib)Piper...")
+  if TARGET == "LINUX" then
     if exists("libpiper") then
       rm("libpiper")
     end
@@ -190,23 +123,9 @@ if TARGET == "LINUX" then
     mv("libpiper/build/pi/include/piper-phonemize/tashkeel.hpp", "include/piper/piper-phonemize/")
     rm("libpiper")
     rm("piper-phonemize*")
+  else
+    todo()
   end
-
-  print("Done.")
-elseif TARGET == "WIN" then
-  print("Download and install raylib (for app only)...")
-  shell("Invoke-WebRequest https://github.com/raysan5/raylib/releases/download/" .. RAYLIB_VERSION .. "/raylib-" .. RAYLIB_VERSION .. "_win64_mingw-w64.zip -OutFile raylib.zip")
-  shell("Expand-Archive -LiteralPath raylib.zip -Destination raylib")
-  mv("raylib/raylib-" .. RAYLIB_VERSION .. "_win64_mingw-w64/lib/libraylib.a", "lib")
-  mv("raylib/raylib-" .. RAYLIB_VERSION .. "_win64_mingw-w64/include/*", "include")
-  rm("raylib.zip")
-  rm("raylib")
-  print("Compile and move nfd...")
-  shell("cd nativefiledialog/build/gmake_windows && make config=release_x64")
-  mv("nativefiledialog/build/lib/Release/x64/libnfd.a", "lib")
-  print("Compile and move libspng...")
-  shell("gcc -c libspng/spng/spng.c -o spng.o")
-  shell("ar rcs lib/libspng.a spng.o")
-  rm("spng.o")
 end
 
+run_install_scripts()
